5f6ca1df97f359e550b6dda92e1e32d27fbf6b10
lindennerd <lindennerd@gmail.com>
2022-10-12 09:22:54 -0300

    footer

---
+++ client/src/components/UI/Footer.tsx
@@ -0,0 +1,10 @@
+export function Footer() {
+  return (
+    <div className="w-full border-t flex justify-center items-center p-4">
+      <span className="text-slate-800">
+        Built by{" "}
+        <a href="http://lindennerd.vercel.app/">Lindennerd &#8482; 2022</a>
+      </span>
+    </div>
+  );
+}
--- client/src/pages/Root.tsx
+++ client/src/pages/Root.tsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { UnauthenticatedNavbar } from "../components/Navbar/Unauthenticated";
+import { Footer } from "../components/UI/Footer";
 
 export default function Root() {
   return (
     <>
       <UnauthenticatedNavbar />
-      <div className="mt-12">
+      <div className="mt-12 w-screen h-screen p-2">
         <Outlet />
       </div>
+      <Footer />
     </>
   );
 }
